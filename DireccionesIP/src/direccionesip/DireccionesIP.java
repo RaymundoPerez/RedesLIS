8daa2c6d47f2dba0b1c42b85b7dfb25d981d9a62
RaymundoPerez <raymundo170@hotmail.com>
2017-04-03 22:58:34 -0500

    Programa direcciones IP

---
+++ DireccionesIP/src/direccionesip/DireccionesIP.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package direccionesip;
+
+import GUI.Interfaz;
+
+/**
+ *
+ * @author sony
+ */
+public class DireccionesIP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Interfaz().setVisible(true);// TODO code application logic here
+    }
+    
+}
